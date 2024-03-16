0d27d71d22bced2419b4dbb8d2c36a3f4082821e
Milgam06 <milgamfruit@gmail.com>
2024-03-17 01:41:06 +0900

    feat(component): add relative volume in Button

--- src/components/common/Button/styled.ts
+++ src/components/common/Button/styled.ts
@@ -7,11 +7,10 @@ export const ButtonWrapper = styled(motion.div)<ButtonProps>`
   outline: none;
   border: none;
   border-radius: 2rem;
-  width: ${({ sizes }) => (sizes ? "40rem" : "30rem")};
-  height: ${({ sizes }) => (sizes ? "8rem" : "6rem")};
+  width: ${({ sizes }) => (sizes ? "60rem" : "30rem")};
+  height: ${({ sizes }) => (sizes ? "10rem" : "6rem")};
   display: flex;
   align-items: center;
   justify-content: center;
-  column-gap: 2rem;
   background: linear-gradient(to right bottom, #0076ff, #7400ff);
 `;
