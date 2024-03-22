86bf79536014a8cc81d0d2f1e1231b06f8924218
MILGAM <93873127+Milgam06@users.noreply.github.com>
2024-03-22 23:54:17 +0900

    feat(component): add animations on Navbar

--- src/components/common/Navbar/index.tsx
+++ src/components/common/Navbar/index.tsx
@@ -4,7 +4,11 @@ import * as S from "./styled";
 export const Navbar: React.FC = () => {
   return (
     <>
-      <S.NavbarContainer>
+      <S.NavbarContainer
+        initial={{ opacity: 0, scale: 0.8 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 1 }}
+      >
         <S.NavbarInnerContainer>
           <Text size={2.2} weight={800} colors="#fff">
             FramerMotion
